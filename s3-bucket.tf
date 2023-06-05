d83c2397c8c05e624471b76070a0f68aa73d8e09
ani-08-00 <67070468+ani-08-00@users.noreply.github.com>
2023-06-05 14:23:39 +0530

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,8 +1,6 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
+module "s3-bucket" {
+  source  = "app.terraform.io/blaze-clan/s3-bucket/aws"
+  version = "2.8.0"
   bucket_prefix= "avanis"
 
 
